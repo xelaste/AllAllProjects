edd537025920d36bfbdfd355085fe6c4a8ee8d51
Alexander Stekolchik <42925789+xelaste@users.noreply.github.com>
2020-11-30 17:37:30 +0200

    jest config

--- BullsandCows/nodejs/spec/jestGlobalSetup.js
+++ BullsandCows/nodejs/spec/jestGlobalSetup.js
@@ -4,6 +4,7 @@ module.exports = async () =>
    process.env.LOG_LEVEL="debug";
    process.env.HTTP_PROXY="genproxy:8080";
    process.env.HTTPS_PROXY="genproxy:8080";
+   process.env["GLOBAL_AGENT_HTTP_PROXY"]="http://genproxy:8080";
    console.log(process.env);
    console.log("########################");
 };
