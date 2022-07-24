329fa41bf902b83f65576064a184430f6e1f365d
astekoclchik <alexander.stekolchik@rbbn.com>
2022-07-24 12:29:01 +0300

    menus ++

--- xelapp/web-ui/main/src/react/xelapp_client/src/controls/Navbar.js
+++ xelapp/web-ui/main/src/react/xelapp_client/src/controls/Navbar.js
@@ -27,33 +27,27 @@ function Navbar() {
     }
   };
 
-  return <>
-    <div class="pos-f-t">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="collapse navbar-collapse" id="navbarText">
-          <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <button class="expand" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-          </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-          </ul>
-          <span class="navbar-text">
-            Navbar text with an inline element
-          </span>
-        </div>
-      </nav>
-    </div>
-  </>;
+  return   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="m-sm-2 navbar-brand" href="#"><span class="navbar-toggler-icon"></span></a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  <form>
+      <input class="m-sm-2" type="search" placeholder="Search"/>
+      <button class="btn btn-outline-success m-sm-2" type="submit">Search</button>
+    </form>
+
+</nav>
 }
 
 export default Navbar;
