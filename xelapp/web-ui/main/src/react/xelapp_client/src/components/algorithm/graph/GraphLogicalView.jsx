b55d759a5ba09e708969e5349e31894283b121dc
astekoclchik <alexander.stekolchik@rbbn.com>
2022-08-04 15:53:42 +0300

    graph view events

--- xelapp/web-ui/main/src/react/xelapp_client/src/components/algorithm/graph/GraphLogicalView.jsx
+++ xelapp/web-ui/main/src/react/xelapp_client/src/components/algorithm/graph/GraphLogicalView.jsx
@@ -1,27 +1,46 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DrawPanel from '../../../controls/DrawPanel';
-class DrawPanel extends React.Component {
+class GraphLogicalView extends React.Component {
 
     constructor(props) {
         super(props);
         this.graph = props.graph;
+        this.state = {
+            loaded:false
+          };
+
     }
-   graph = {};
-   drawGraph ()
-   {
 
-   }
-   getCanvas()
-   {
-       return this.canvas; 
-   }
+    graph = {};
+    drawGraph() {
 
-   render ()
-   {
-       return <>
-        <div id="graphLogicalContainer" style={{width:"50%",height:"70%"}}><DrawPanel parentObject={this} conainerId="graphLogicalContainer" title={this.props.title}></DrawPanel> </div>
-       </>
-   }
+    }
+    getCanvas() {
+        return this.canvas;
+    }
 
+    render() {
+        return <>
+            <div id="graphLogicalContainer" style={{ width: "50%", height: "70%" }}><DrawPanel parentObject={this} conainerId="graphLogicalContainer" title={this.props.title}></DrawPanel> </div>
+        </>
+    }
+    
+    handleResize() {
+        let size = {width:window.innerWidth,height:window.innerHeight };
+        this.state = {
+            size: size
+          };
+     }
+    componentDidMount() {
+        this.state = {
+            ...this.state,
+            loaded:true
+          };
+          window.addEventListener('resize', handleResize);
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) 
+    {
+    }
 }
+export default GraphLogicalView;
