d95fa156adb8e22fcf73b5584788bcf9b753cd46
alexander <xelaste@gmail.com>
2024-04-11 16:18:23 +0300

    sort add merge

--- xelapp/web-ui/main/src/react/xelapp_client/src/modules/algorithm/sort/impl/Sorts.js
+++ xelapp/web-ui/main/src/react/xelapp_client/src/modules/algorithm/sort/impl/Sorts.js
@@ -9,6 +9,73 @@ export default function Sorts(props) {
     const [execution, setExecution] = useState(false);
     const [state, setState] = useState({});
     let content=defaultContent;
+    
+    async function mergeSortCall()
+    {
+        setExecution(true)
+        await mergeSort( 0 , content.length - 1);
+        setExecution(false)
+    }
+    async function mergeSort( from , to)
+    {
+        if (from===to)
+        {
+            state.complexity+=2;
+            let newState = {
+                ...state,
+            }
+            setState(newState);
+            return [content[from]];
+        }
+        else if (from+1===to) {
+            let arr = []
+            arr[0] = content[from]<content[to]?content[from]:content[to]
+            arr[1] = content[from]>=content[to]?content[from]:content[to]
+            state.complexity+=5;
+            let newState = {
+                ...state
+            }
+            setState(newState);
+            return arr;  
+        }       
+        let halfIdx = Math.floor((from + to)/2);
+        let half1 = await mergeSort (from,halfIdx);
+        let half2 = await mergeSort (halfIdx + 1 , to);
+        let idx1=0
+        let idx2=0
+        state.complexity+=3;
+        for (let i=0;i<half1.length + half2.length;i++)
+        {
+            let idx = i + from;
+            if (idx1 < half1.length && idx2 < half2.length) {
+                if (half1[idx1] < half2[idx2])
+                {
+                    content[idx]=half1[idx1]
+                    idx1++;                        
+                } 
+                else
+                {
+                    content[idx]=half2[idx2];
+                    idx2++;    
+                }
+            } else if (idx1 >= half1.length) {
+                content[idx]=half2[idx2];
+                idx2++;    
+            } else {
+                content[idx]=half1[idx1]
+                idx1++;                        
+            }
+        
+            state.complexity+=5;
+            let newState = {
+                ...state
+            }
+            setState(newState);
+            await sleep(30);
+        }
+        return content.slice(from,to + 1);
+    }
+
     async function bubbleSort()
     {
         setExecution (true);
@@ -76,6 +143,7 @@ export default function Sorts(props) {
         let newState = {
             ...state,
             content:content,
+            start:Math.floor(Date.now() / 1000),
             complexity:0,
             selectedSort:e.target.value
         }
@@ -95,6 +163,15 @@ export default function Sorts(props) {
 
     function sort()
     {
+        state.complexity=0;
+        state.start=Math.floor(Date.now() / 1000);
+        let newState = {
+            ...state,
+            complexity: 0,
+            start: Math.floor(Date.now() / 1000),
+            content:content
+        }
+        setState(newState);
         switch (state.selectedSort) { 
           case "1":
             bubbleSort();
@@ -103,7 +180,7 @@ export default function Sorts(props) {
             insertSort();
             break;
           case "3":
-            
+            mergeSortCall();
             break;
           case "4":
             
@@ -150,8 +227,8 @@ export default function Sorts(props) {
                             <div className="card-body">
                                 <form>
                                     <label className="mb-1" htmlFor="sortTypesList">Sorting algorithms</label>
-                                    <select disabled={execution} id="sortTypesList" onChange={onSortSelect} className="form-select form-label form-select-lg mb-3">
-                                        <option value="Please Specify" selected>Select Sort Algorithm</option>
+                                    <select defaultValue="Please Specify" disabled={execution} id="sortTypesList" onChange={onSortSelect} className="form-select form-label form-select-lg mb-3">
+                                        <option value="Please Specify">Select Sort Algorithm</option>
                                         <option value="1">Bubble Sort</option>
                                         <option value="2">Insert Sort</option>
                                         <option value="3">Merge Sort</option>
@@ -168,6 +245,7 @@ export default function Sorts(props) {
                                             let newState = {
                                                 ...state,
                                                 complexity:0,
+                                                start:Math.floor(Date.now() / 1000),
                                                 content:content
                                             }
                                             setState(newState);
@@ -180,7 +258,9 @@ export default function Sorts(props) {
                                         type="button"
                                         disabled={execution || !isValidSort()}
                                         style={{ width: "6em" }}
-                                        onClick={sort}
+                                        onClick={(e)=>{
+                                            sort();
+                                        }}
                                         className="btn btn-primary mt-2 mx-2"
                                     >
                                         Run
@@ -209,7 +289,12 @@ export default function Sorts(props) {
                                         <span className="m-1">{state.complexity}</span>
                                     </div>
                                 </div>
-
+                                <div className="row row-eq-height align-items-end">
+                                    <div className="col">
+                                        <label>Elapsed Time</label>
+                                        <span className="m-1">{state.start?Math.floor(Date.now()/1000)-state.start:""}</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
@@ -219,7 +304,7 @@ export default function Sorts(props) {
                         <div
                             id="algorithmContainer"
                             style={{ width: "100%", height: "80%" }}
-                            class="overflow-auto"
+                            className="overflow-auto"
                         >
                             <SortLogicalView title={state.selectedSort} content={content}></SortLogicalView>
                         </div>
